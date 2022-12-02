c4293f39f50c076cbcfe6329e4d8b5d388d528e2
Enirose <enirosehellum@gmail.com>
2022-12-02 02:22:06 +0100

    added login url

--- src/js/api/auth/constants.mjs
+++ src/js/api/auth/constants.mjs
@@ -1,3 +1,7 @@
 export const BASE_URL = `https://api.noroff.dev/api/v1`;
 export const register_URL = `/social/auth/register`;
 export const API_SOCIAL_URL = `${BASE_URL}${register_URL}`;
+
+// login url
+export const login_URL = `/social/auth/login`;
+export const API_LOGIN_URL = `${BASE_URL}${login_URL}`;
