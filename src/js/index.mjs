363975d3c3debae00bdd901c10e0e98b86bd2830
Enirose <enirosehellum@gmail.com>
2022-12-07 22:47:03 +0100

    add API posts method

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,10 +1,20 @@
 import { registerForm } from "./handlers/register.mjs";
 import { loginUser } from "./handlers/login.mjs";
+import * as post from "./posts/index.mjs"
+import { getPosts } from "./posts/index.mjs";
 
-const path = location.pathname;
+const path = window.location.pathname;
 
 if (path === '/index.html'){
     loginUser();
 } else if (path === '/registration.html'){
     registerForm();
 }
+
+// post.createPost()
+// post.updatePost()
+// post.removePost()
+// post.getPosts().then(console.log)
+// post.get_specificPost()
+
+post.get_specificPost(614).then(console.log)
